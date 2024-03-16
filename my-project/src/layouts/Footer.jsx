28d89e420460846dec2422b5566a75e18fbf43e2
Amartya Ghosh <amartyaghosh48@gmail.com>
2024-03-17 00:23:24 +0530

    some changes

--- my-project/src/layouts/Footer.jsx
+++ my-project/src/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='h-max justify-between bg-black flex flex-col items-center justify-center'>
+    <footer className='h-max bg-black flex flex-col items-center justify-center'>
       <p className='text-white/50'>Copyright © 2024 | Techtester Infotech</p>
     </footer>
   )
